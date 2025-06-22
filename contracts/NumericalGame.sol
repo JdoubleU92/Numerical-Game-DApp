583c4e0939d1138a0b01a4ae4b064266bc429db2
JdoubleU92 <110284132+JdoubleU92@users.noreply.github.com>
2025-06-22 23:58:53 +0200

    Update NumericalGame.sol

--- contracts/NumericalGame.sol
+++ contracts/NumericalGame.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/utils/Strings.sol"; // Library, daher keine Vererbung erforderlich
 
